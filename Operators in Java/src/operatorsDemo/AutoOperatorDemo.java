af02715f5c8177e702d273604dbc116e5c3d2aea
RaghuBN <raghu.astepahead@gmail.com>
2019-11-05 23:10:28 +0530

    Operatos in Java

---
+++ Operators in Java/src/operatorsDemo/AutoOperatorDemo.java
@@ -0,0 +1,15 @@
+//Auto-increment and Auto-decrement Operators
+package operatorsDemo;
+
+public class AutoOperatorDemo 
+{
+	 public static void main(String args[])
+	 {
+	      int num1=100;
+	      int num2=200;
+	      num1++;
+	      num2--;
+	      System.out.println("num1++ is: "+num1);
+	      System.out.println("num2-- is: "+num2);
+	 }
+}
